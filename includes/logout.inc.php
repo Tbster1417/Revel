171951e89c3ebfbf5b5a9dc052d64d65307eddc5
Tbster1417 <tbster1417@yahoo.com>
2018-02-15 09:25:56 -0800

    Update logout.inc.php

--- includes/logout.inc.php
+++ includes/logout.inc.php
@@ -1,13 +1,8 @@
 <?php
 if(isset($_POST['signout'])){
-	/*session_destroy('u_id');
-	session_destroy('u_first');
-	session_destroy('u_last');
-	session_destroy('u_uid');
-	session_destroy('u_email');*/
-	session_start();
+    session_start();
     $_SESSION = array();
     session_destroy();
-	header('Location: ../index.php');
+    header('Location: ../index.php');
 }
 ?>
